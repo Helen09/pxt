58dce6545c3eb11971924242a0b1dfae6b125450
Steven Huang <v-jianch@microsoft.com>
2019-11-20 16:47:32 +0800

    Add testing code of Turtle module

---
+++ tests/ui-test/src/turtle/square.js
@@ -0,0 +1,46 @@
+import { DomObject } from '../lib/dom-object';
+import assert from "assert";
+let { turtle, commonActions } = require('../lib/css-value');
+
+class Square extends DomObject {
+
+    async square() {
+
+        await this.click(turtle.square, commonActions.closeButton,
+            turtle.square, commonActions.startTutorial);
+
+        let headerTitle = await this.getText(commonActions.headerTitle);
+        assert.equal(headerTitle, 'Turtle Square');
+        console.debug(`The title of the alert window is "${headerTitle}"`);
+        
+        await this.click(commonActions.okButton);
+
+        for (let i = 1; i < 5; i++) {
+
+            await this.click(commonActions.goNextButton);
+            
+            let cssValueOfSelectLabel = await this.getAttribute(commonActions.selectedLabel, 'class');
+            assert.equal(cssValueOfSelectLabel, 'ui circular label blue selected ');
+
+            let selectLabel = await this.getAttribute(commonActions.selectedLabel, 'aria-label');
+            console.log(selectLabel);
+        }
+
+        await this.click(commonActions.finishButton);
+       
+        let projectName = await this.getAttribute(commonActions.projectName, 'value');
+        assert.equal(projectName, 'Square');
+        console.debug(`The current project name is "${projectName}"`);
+       
+        await this.click(commonActions.microbitLogo);
+
+    }
+
+    test() {
+        it('Start learning the "Square"', async () => {
+            return await this.square();
+        });
+    }
+
+}
+export let square = new Square();
