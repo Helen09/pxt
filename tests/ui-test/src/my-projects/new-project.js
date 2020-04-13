c503199e49de1f00dde99e75cf01c316034c219b
Steven Huang <v-jianch@microsoft.com>
2020-04-13 09:48:06 +0000

    Add python module and add test cases for new options of creating project

--- tests/ui-test/src/my-projects/new-project.js
+++ tests/ui-test/src/my-projects/new-project.js
@@ -7,20 +7,57 @@ class NewProjectPage extends DomObject {
     async getCodeSource() {
 
         console.log("Start test getCodeSource()");
+        for (let i = 1; i < 4; i++) {
+            await this.click(newProject.newProjectButton);
 
-        await this.click(newProject.newProjectButton, newProject.homeOfProject, newProject.closeButtonOfHomePage);
+            if (i == 1) {
+                await this.click(newProject.codeOptions, newProject.selectBox, newProject.pythonOnly);
 
-        await this.click(newProject.homeOfProject);
+                let selectOption = await this.getAttribute(newProject.pythonOnly, newProject.isSelect);
+                assert.equal(selectOption, 'true');
+                console.log(`The select status is:${selectOption}`);
 
-        await this.sendKeys(newProject.inputProjectName, 'Project1');
+                await this.click(newProject.saveButtonOfHomePage);
 
-        await this.click(newProject.saveButtonOfHomePage);
+                let pythonLabel = await this.getAttribute(newProject.centerItem, newProject.itemLabel);
+                assert.equal(pythonLabel, 'Python');
+                console.log(`The current label is :${pythonLabel}`);
+                
+                await this.click(newProject.homeOfProject, newProject.skipButton);
 
-        let text = await this.getText(newProject.projectTitle);
-        
-        assert.equal(text, "Project1");
-       
-        console.debug(`The header text in the first DIV of 'My Projects' is: ${text}"`);
+            }
+            if (i == 2) {
+                await this.click(newProject.codeOptions, newProject.selectBox, newProject.javaScriptOnly);
+
+                let selectOption = await this.getAttribute(newProject.javaScriptOnly, newProject.isSelect);
+                assert.equal(selectOption, 'true');
+                console.log(`The select status is:${selectOption}`);
+
+                await this.click(newProject.saveButtonOfHomePage);
+
+                let javascriptLabel = await this.getAttribute(newProject.centerItem, newProject.itemLabel);
+                assert.equal(javascriptLabel, 'JavaScript');
+                console.log(`The current label is :${javascriptLabel}`);
+
+                await this.click(newProject.homeOfProject, newProject.skipButton);
+
+            }
+            if (i == 3) {
+                await this.click(newProject.saveButtonOfHomePage, newProject.homeOfProject, newProject.closeButtonOfHomePage);
+
+                await this.click(newProject.homeOfProject);
+
+                await this.sendKeys(newProject.inputProjectName, 'Project1');
+
+                await this.click(newProject.saveButtonOfHomePage);
+
+                let text = await this.getText(newProject.projectTitle);
+
+                assert.equal(text, "Project1");
+
+                console.debug(`The header text in the first DIV of 'My Projects' is: ${text}"`);
+            }
+        }
 
     }
 
--- tests/ui-test/src/my-projects/toggle-editor.js
+++ tests/ui-test/src/my-projects/toggle-editor.js
@@ -16,6 +16,13 @@ class ToggleDisplayForm extends DomObject {
 
         console.debug(`The ${explorerToolbar} toolbar appears after toggling to JavaScript option`);
 
+        await this.click(switchButton.dropDownMenu, switchButton.switchToPython);
+
+        await driver.sleep(2000);
+        let firstLineOfPython = await this.getText(switchButton.pythonSrc1);
+        assert.equal(firstLineOfPython, 'def');
+        console.log(`The first line of python script is :${firstLineOfPython}`);
+
         await this.click(switchButton.switchToBlocks);
 
     }
